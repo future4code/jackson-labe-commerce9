5c5eeed43034edfab3482ed193990bb2cb032628
Astor Lohmann Palmeira <astorlohmann@gmail.com>
2020-08-19 14:56:36 -0300

    projeto base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ContainerProduto from './components/container-produto';
 
-function App() {
+class App extends React.Component() {
+  state = {
+    listaDeProdutos: [
+        {
+            id: 1,
+            name: "Foguete da Missão Apollo 11",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/200",
+        };
+        {
+          id: 2,
+          name: "Foguete da Missão Apollo 11",
+          value: 500.0,
+          imageUrl: "https://picsum.photos/200/201",
+        };
+        {
+          id: 3,
+          name: "Foguete da Missão Apollo 11",
+          value: 250.0,
+          imageUrl: "https://picsum.photos/200/202",
+        };
+        {
+          id: 4,
+          name: "Foguete da Missão Apollo 11",
+          value: 3000.0,
+          imageUrl: "https://picsum.photos/200/203",
+       };
+       {
+        id: 5,
+        name: "Foguete da Missão Apollo 11",
+        value: 5000.0,
+        imageUrl: "https://picsum.photos/200/204",
+       };
+       {
+        id: 6,
+        name: "Foguete da Missão Apollo 11",
+        value: 600.0,
+        imageUrl: "https://picsum.photos/200/205",
+       };
+       {
+        id: 7,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/200/206",
+    };
+      {
+        id: 8,
+        name: "Foguete da Missão Apollo 11",
+        value: 10000.0,
+        imageUrl: "https://picsum.photos/200/207",
+    };
+    ]
+}
+ 
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ContainerProduto
+     id=1
+     >
+     <ContainerProduto
+     id=2
+     >
+     <ContainerProduto
+     id=3
+     >
+     <ContainerProduto
+     id=4
+     >
+     <ContainerProduto
+     id=5
+     >
+     <ContainerProduto
+     id=6
+     >
+     <ContainerProduto
+     id=7
+     >
+     <ContainerProduto
+     id=8
+     >
     </div>
   );
 }
---
+++ src/components/container-produto.js
@@ -0,0 +1,18 @@
+
+
+
+
+class ContainerProduto extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.imageURL} />
+                <p>{this.props.name</p>
+                <p>this.props.value</p>
+                <button>Adicionar ao Carrinho</button>
+            </div>
+        );
+    }
+}
+
+export default ContainerProduto
