b643b262d54e4d915125d6ccca4cabb457790dfe
Astor Lohmann Palmeira <astorlohmann@gmail.com>
2020-08-20 11:54:22 -0300

    homepagepronta

--- src/App.js
+++ src/App.js
@@ -1,72 +1,78 @@
 import React from 'react';
 import './App.css';
 import ContainerProduto from './components/ContainerProduto';
-import Filtro from './components/Filtro';
 
-class App extends React.Component() {
-  state = {
-    listaDeProdutos: [
-        {
-            id: 1,
+  export default class App extends React.Component {
+    state = {
+      listaDeProdutos: [
+          {
+              id: 1,
+              name: "Foguete da Missão Apollo 11",
+              value: 10000.0,
+              imageUrl: "https://picsum.photos/200/200",
+          },
+          {
+            id: 2,
+            name: "Foguete da Missão Apollo 11",
+            value: 500.0,
+            imageUrl: "https://picsum.photos/200/201",
+          },
+          {
+            id: 3,
+            name: "Foguete da Missão Apollo 11",
+            value: 250.0,
+            imageUrl: "https://picsum.photos/200/202",
+          },
+          {
+            id: 4,
+            name: "Foguete da Missão Apollo 11",
+            value: 3000.0,
+            imageUrl: "https://picsum.photos/200/203",
+         },
+         {
+            id: 5,
+            name: "Foguete da Missão Apollo 11",
+            value: 5000.0,
+            imageUrl: "https://picsum.photos/200/204",
+         },
+         {
+            id: 6,
+            name: "Foguete da Missão Apollo 11",
+            value: 600.0,
+            imageUrl: "https://picsum.photos/200/205",
+         },
+         {
+            id: 7,
             name: "Foguete da Missão Apollo 11",
             value: 10000.0,
-            imageUrl: "https://picsum.photos/200/200",
-        },
+            imageUrl: "https://picsum.photos/200/206",
+      },
         {
-          id: 2,
-          name: "Foguete da Missão Apollo 11",
-          value: 500.0,
-          imageUrl: "https://picsum.photos/200/201",
-        },
-        {
-          id: 3,
-          name: "Foguete da Missão Apollo 11",
-          value: 250.0,
-          imageUrl: "https://picsum.photos/200/202",
-        },
-        {
-          id: 4,
-          name: "Foguete da Missão Apollo 11",
-          value: 3000.0,
-          imageUrl: "https://picsum.photos/200/203",
-       },
-       {
-        id: 5,
-        name: "Foguete da Missão Apollo 11",
-        value: 5000.0,
-        imageUrl: "https://picsum.photos/200/204",
-       },
-       {
-        id: 6,
-        name: "Foguete da Missão Apollo 11",
-        value: 600.0,
-        imageUrl: "https://picsum.photos/200/205",
-       },
-       {
-        id: 7,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/206",
-    },
-      {
-        id: 8,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/207",
-    },
-    ]
-}
- 
- 
- render(){
-  return (
-  <ContainerProduto
-   listaDeProdutos id="1"
-  
-  />
-    
-    
-  )};
-}
+            id: 8,
+            name: "Foguete da Missão Apollo 11",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/207"
+      },
+      ],
+    }
 
-export default App;
+
+ 
+
+  render(){
+    
+      const produtos = this.state.listaDeProdutos.map((produtos) => {
+        return (
+          <ContainerProduto
+            name={produtos.name}
+            value={produtos.value}
+            imageUrl={produtos.imageUrl}
+          />
+        )
+      })
+      return (
+        <div>
+          {produtos}
+        </div>
+    );
+  }}
--- src/components/ContainerProduto.js
+++ src/components/ContainerProduto.js
@@ -6,7 +6,7 @@ class ContainerProduto extends React.Component {
     render() {
         return (
             <div>
-                <img src={this.props.imageURL} />
+                <img src={this.props.imageUrl} alt={"imagem do produto"} />
                 <p>{this.props.name}</p>
                 <p>{this.props.value}</p>
                 <button>Adicionar ao Carrinho</button>
